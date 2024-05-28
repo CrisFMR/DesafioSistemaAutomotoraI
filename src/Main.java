658422136b64b8ce7cc5450c412dd4c23eb5882a
CrisFMR <christopher.montecinos@live.com>
2024-05-27 23:57:37 -0400

    Se adjunta IMG perteneciente al desafio

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        //Mensaje bienvenida
+        System.out.println("======    ======        ====    ===     ===  ===     =====  ");
+        System.out.println("=======   =======      ==  ==    ===   ===   ===    ==   == ");
+        System.out.println("==    ==  ==     ==   ===  ===    === ===    ===   ===     =");
+        System.out.println("==    ==  ==     ==   ===  ===      ===      ===    =====   ");
+        System.out.println("=======   =======     ========     =====     ===      ===== ");
+        System.out.println("====      ===  ===    ========    === ===    ===   =     ===");
+        System.out.println("===       ===   ===   ===  ===   ===   ===   ===    ==   == ");
+        System.out.println("===       ===    ===  ===  ===  ===     ===  ===     =====  ");
+        System.out.println("____________________________________________________________");
+        System.out.println("============     Desafio Sitema Automotora I.   ============");
+        System.out.println("____________________________________________________________");
+
+        //Imagen se encuentra dentro de carpeta IMG. Ruta: src/IMG
+    }
+}
